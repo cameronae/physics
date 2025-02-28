db67e82987151f83a4f6f16f03243af153e17177
cameronae <138916869+cameronae@users.noreply.github.com>
2025-02-27 23:28:05 -0500

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -3,8 +3,8 @@
         const b = Math.floor(Math.random() * (70 - 30 + 1)) + 30; // Random number between 3 and 5
         const radians = b * (Math.PI / 180);
         // Display random values for a and b
-        document.getElementById('velo').textContent = a;
-        document.getElementById('theta').textContent = b;
+        document.getElementById("velo").textContent = a;
+        document.getElementById("theta").textContent = b;
         
         // Answers
         
